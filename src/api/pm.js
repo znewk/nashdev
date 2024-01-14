a4be41a3008d94c5d7080e25eb23d6df83b148eb
nbekposynov <nbekposynov@gmail.com>
2024-01-14 17:27:58 +0600

    Update

--- src/api/pm.js
+++ src/api/pm.js
@@ -36,7 +36,32 @@ class PmAPI {
     };
 
 
+    
+    // Метод getAllRequests
     getAllRequests = async (id) => {
+
+        const updateStatuses = (statusId) => {
+            const statuses = [
+                { title: 'Модерация', status: '' },
+                { title: 'Поиск исполнителей', status: '' },
+                { title: 'Утверждение исполнителей', status: '' },
+                { title: 'Оплата', status: '' },
+                { title: 'В работе', status: '' },
+                { title: 'Завершение', status: '' },
+            ];
+        
+            statuses.forEach((status, index) => {
+                if (index < statusId - 1) {
+                    status.status = 'Успешно';
+                } else if (index === statusId - 1) {
+                    status.status = 'Ожидает';
+                }
+            });
+        
+            return statuses;
+        };
+
+
         try {
             const token = localStorage.getItem('token');
             if (!token) {
@@ -57,11 +82,49 @@ class PmAPI {
             }
     
             const responseData = await response.json();
-            return responseData.requests
+            
+            // Обновляем каждый запрос с учетом его текущего status_id
+            const updatedRequests = responseData.requests.map(request => {
+                return {
+                    ...request,
+                    statuses: updateStatuses(request.status_id)
+                };
+            });
+    
+            console.log("Обновленные данные запросов:", updatedRequests); // Логирование обновленных данных
+            return updatedRequests;
+    
         } catch (error) {
             console.error('Ошибка при выполнении запроса', error);
         }
     };
+
+    // getAllRequests = async (id) => {
+    //     try {
+    //         const token = localStorage.getItem('token');
+    //         if (!token) {
+    //             console.error('Отсутствует токен авторизации');
+    //             return;
+    //         }
+    //         const headers = new Headers({
+    //             'Authorization': `Bearer ${token}`,
+    //             'Content-Type': 'application/json',
+    //         });
+    
+    //         const response = await fetch(`${API_BASE_URL}/getAllRequests `, {
+    //             method: 'POST', 
+    //             headers: headers
+    //         });
+    //         if (!response.ok) {
+    //             throw new Error(`Ошибка HTTP: ${response.status}`);
+    //         }
+    
+    //         const responseData = await response.json();
+    //         return responseData.requests
+    //     } catch (error) {
+    //         console.error('Ошибка при выполнении запроса', error);
+    //     }
+    // };
 
     getManagerTasksByRequest = async (id) => {
         try {
